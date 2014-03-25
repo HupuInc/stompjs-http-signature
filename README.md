af6040acb15b4263433eb7c73e8b016bc252d2bb
Wang Wenlin <sopl.wang@gmail.com>
2014-03-25 12:47:10 +0800

    Initial import

---
+++ README.md
@@ -0,0 +1,8 @@
+Stomp.js with http-signature support
+====================================
+
+Usage:
+
+```javascript
+Stomp.overWS = require('stompjs-http-signature').overWS;
+```
---
+++ index.js
@@ -0,0 +1,79 @@
+/*
+ Copyright 2014 Hupu Inc., by Wang Wenlin <wangwenlin@hupu.com>
+ BSD License.
+ */
+
+var httpSignature = require('http-signature');
+
+/* HOWTO:
+  Stomp.overWS = require('stompjs-http-signature').overWS;
+ */
+exports.overWS = function (url, options) {
+  var socket;
+  socket = wrapWS(url, options ? sign(url, options) : {});
+  return this.over(socket);
+};
+
+function sign(url, options) {
+  if (typeof url === 'string') {
+    url = require('url').parse(url);
+  }
+
+  var dummyreq = {
+    method: 'GET',
+    path: url.path,
+    headers: {},
+
+    setHeader: function (k, v) {
+      return this.headers[k.toLowerCase()] = v;
+    },
+    getHeader: function (k) {
+      return this.headers[k.toLowerCase()];
+    },
+  };
+
+  if (httpSignature.sign(dummyreq, options)) {
+    return dummyreq.headers;
+  } else {
+    return {};
+  }
+}
+
+function wrapWS(url, headers) {
+  var WebSocketClient = require('websocket').client;
+  var connection = null;
+
+  var ws = {
+    url: url,
+    send: function(d) {
+      return connection.sendUTF(d);
+    },
+    close: function() {
+      return connection.close();
+    },
+  };
+
+  var socket = new WebSocketClient();
+  socket.on('connect', function(conn) {
+    connection = conn;
+    ws.onopen();
+    connection.on('error', function(error) {
+      return typeof ws.onclose === "function" ? ws.onclose(error) : void 0;
+    });
+    connection.on('close', function() {
+      return typeof ws.onclose === "function" ? ws.onclose(error) : void 0;
+    });
+    return connection.on('message', function(message) {
+      var event;
+      if (message.type === 'utf8') {
+        event = {
+          'data': message.utf8Data
+        };
+        return ws.onmessage(event);
+      }
+    });
+  });
+
+  socket.connect(url, [], '', headers);
+  return ws;
+};
