4f8cad5477f4330b4e9ff48145f21d5ff9873a6f
Wang Wenlin <sopl.wang@gmail.com>
2014-03-28 15:24:31 +0800

    Improve error handling

--- README.md
+++ README.md
@@ -6,9 +6,13 @@ Usage:
 ```javascript
 Stomp.overWS = require('stompjs-http-signature').overWS;
 
-Stomp.overWS('ws://127.0.0.1:3000/', {
-  algorithm: 'hmac-sha256',
-  keyId: 'id',
-  key: 'key',
+var client = Stomp.overWS('ws://127.0.0.1:3000', {
+  onerror: function onerr(e) {},
+  onclose: function onclose() {},
+  sign: {
+    algorithm: 'hmac-sha256',
+    keyId: 'id',
+    key: 'key',
+  },
 });
 ```
--- index.js
+++ index.js
@@ -3,15 +3,27 @@
  BSD License.
  */
 
+var WebSocketClient = require('websocket').client;
 var httpSignature = require('http-signature');
 
 /* HOWTO:
   Stomp.overWS = require('stompjs-http-signature').overWS;
+
+  var client = Stomp.overWS('ws://127.0.0.1:3000', {
+    onerror: function onerr(e) {},
+    onclose: function onclose() {},
+    sign: {
+      algorithm: 'hmac-sha256',
+      keyId: 'id',
+      key: 'key',
+    },
+  });
  */
 exports.overWS = function (url, options) {
-  var socket;
-  socket = wrapWS(url, options ? sign(url, options) : {});
-  return this.over(socket);
+  if (!options) options = {};
+  if (!options.headers) options.headers = {};
+  if (options.sign) sign(url, options);
+  return this.over(wrapWS(url, options));
 };
 
 function sign(url, options) {
@@ -19,10 +31,10 @@ function sign(url, options) {
     url = require('url').parse(url);
   }
 
-  var dummyreq = {
+  httpSignature.sign({
     method: 'GET',
     path: url.path,
-    headers: {},
+    headers: options.headers,
 
     setHeader: function (k, v) {
       return this.headers[k.toLowerCase()] = v;
@@ -30,17 +42,11 @@ function sign(url, options) {
     getHeader: function (k) {
       return this.headers[k.toLowerCase()];
     },
-  };
-
-  if (httpSignature.sign(dummyreq, options)) {
-    return dummyreq.headers;
-  } else {
-    return {};
-  }
+  }, options.sign);
 }
 
-function wrapWS(url, headers) {
-  var WebSocketClient = require('websocket').client;
+function wrapWS(url, options) {
+  var socket = new WebSocketClient();
   var connection = null;
 
   var ws = {
@@ -53,16 +59,21 @@ function wrapWS(url, headers) {
     },
   };
 
-  var socket = new WebSocketClient();
   socket.on('connect', function(conn) {
+    if (typeof options.onopen === "function") options.onopen(conn);
+
     connection = conn;
     ws.onopen();
+
     connection.on('error', function(e) {
+      if (typeof options.onerror === "function") options.onerror(e);
       return typeof ws.onclose === "function" ? ws.onclose(e) : void 0;
     });
     connection.on('close', function() {
+      if (typeof options.onclose === "function") options.onclose();
       return typeof ws.onclose === "function" ? ws.onclose() : void 0;
     });
+
     return connection.on('message', function(message) {
       var event;
       if (message.type === 'utf8') {
@@ -74,6 +85,10 @@ function wrapWS(url, headers) {
     });
   });
 
-  socket.connect(url, [], '', headers);
+  socket.on('connectFailed', function(e) {
+    if (typeof options.onerror === "function") options.onerror(e);
+  });
+
+  socket.connect(url, [], '', options.headers);
   return ws;
 };
