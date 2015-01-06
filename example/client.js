eada39068425d34a030384d7480c737e303ce345
dulei <dulei@hupu.com>
2015-01-06 15:01:15 +0800

    Update client example

--- example/client.js
+++ example/client.js
@@ -4,9 +4,7 @@ var Stomp = require('stompjs');
 Stomp.overWS = require('../').overWS;
 
 (function process() {
-  var client = Stomp.overWS('ws://127.0.0.1:3000', {
-    onerror: onend,
-    onclose: onend,
+  var client = Stomp.overWS('ws://127.0.0.1:3000/stomp', {
     sign: {
       algorithm: 'hmac-sha256',
       keyId: 'id',
@@ -14,28 +12,23 @@ Stomp.overWS = require('../').overWS;
     },
   });
 
-  var timer = setTimeout(function () {
-    client.disconnect();
-    onend();
-  }, 5*1000);
-
-  client.debug = console.log.bind(console);
-
-  client.connect({}, function onopen() {
-    clearTimeout(timer);
-
+  function onopen() {
     client.subscribe('/topic/foo', function (msg) {
       console.log(msg.body);
       msg.ack();
     }, {id: 'ex1.foo', persistent: true, ack: 'client'});
-  });
+  }
 
   function onend(e) {
     if (e) console.log(e);
     if (client) {
       client = null;
+      // try to reconnect when it's closed
       setTimeout(process, 1000);
     }
   }
+
+  client.debug = console.log.bind(console);
+  client.connect({}, onopen, onend);
 
 })();
